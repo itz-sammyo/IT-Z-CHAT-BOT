c01ddcfe25165e3ed0c71df80ff2afd438f65e3e
itz-sammyo <itzxsammyo33@gmail.com>
2026-03-04 17:15:59 +0600

    Update pair.js

--- Script/commands/pair.js
+++ Script/commands/pair.js
@@ -55,6 +55,6 @@ module.exports.run = async function ({ event, api, Users }) {
     );
 
   } catch (err) {
-    return api.sendMessage("API Error Call Boss SAHU", threadID, messageID);
+    return api.sendMessage("API Error Call Boss It'z Sammyo", threadID, messageID);
   }
 };
