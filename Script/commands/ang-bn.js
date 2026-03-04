7fe787ee286d1e71f10736ae977815fe597fe395
itz-sammyo <itzxsammyo33@gmail.com>
2026-03-04 15:24:34 +0600

    Update credits in ang-bn.js

--- Script/commands/ang-bn.js
+++ Script/commands/ang-bn.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "bn",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "Islamick Cyber Chat",
+  credits: "It'z Sammyo",
   usePrefix: false,
   description: "Text translation",
   commandCategory: "media",
