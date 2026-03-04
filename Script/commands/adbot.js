c4721768954ef2618a1cc82513ece5d449c05bf2
itz-sammyo <itzxsammyo33@gmail.com>
2026-03-04 15:12:09 +0600

    Update adbot.js

--- Script/commands/adbot.js
+++ Script/commands/adbot.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "ckbot",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔",
+    credits: "It'z Sammyo",
     description: "DESCRIPTION ABOUT BOT",
     commandCategory: "Media",
     usages: "",
@@ -54,7 +54,7 @@ module.exports.run = async ({ api, event, args }) => {
     if (args[0] == "admin") {
         let callback = () => api.sendMessage(
             {
-                body: `———»ADMIN BOT«———\n❯ Name: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔\n❯ Facebook: https://facebook.com/100044713412032\n❯ Thanks for using ${global.config.BOTNAME} bot`,
+                body: `———»ADMIN BOT«———\n❯ Name: It'z Sammyo\n❯ Facebook: https://www.facebook.com/itzXsammyo/n❯ Thanks for using ${global.config.BOTNAME} bot`,
                 attachment: fs.createReadStream(__dirname + "/cache/1.png")
             },
             event.threadID,
